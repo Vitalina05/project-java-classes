a81f314902969d72717973b53f700c5bcdf88e7e
Vitalina <vityljazybko@gmail.com>
2018-04-02 23:51:05 +0300

    try to fix task

--- src/homeTasks/arrayAverage.java
+++ src/homeTasks/arrayAverage.java
@@ -5,25 +5,17 @@ public class arrayAverage {
 
     public static void main(String argv[]) {
 
-//        int a = 2;
-//        int b = 2;
-//        a = 3;
-//        int c = a + b;
+        int [] arr = new int[5];
+        int l = arr.length;
 
-        Object arr[] = new Object[8];
-        arr[0] = new Object();
-        arr[1] = null;
-        arr[2] = arr;
-        arr[3] = 3;
-        arr[4] = 0;
-        arr[5] = 4;
-        arr[6] = 12;
-        arr[7] = 23;
-        arr[8] = 2;
+        arr[0] = 2;
+        arr[1] = 2;
+        arr[2] = 2;
+        arr[3] = 2;
+        arr[4] = 2;
+       // int length = arr.length;
+        int arrayValue= (arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5])/l;
 
-
-        int len = arr.length;
-
-        System.out.println(len);
+        System.out.println(arrayValue);
     }
 }
