eb52365d5e87ad7c1eb5b9d9df77fcddfcc45e5f
vitalina <vitalina.zubko@mev.com>
2018-04-05 11:38:44 +0300

    finally fix fibonacci sequence

--- src/homeTasks/FibonacciNumber.java
+++ src/homeTasks/FibonacciNumber.java
@@ -1,17 +1,29 @@
 package homeTasks;
+import java.util.Scanner;
 
 public class FibonacciNumber {
 
     public static void main(String argv[]) {
         int prev = 0;
         int curr = 1;
-        int n = 12;
+        int n=0;
 
-        for (int i = 0; i < n; i++) {
-            int next = prev+curr;
-            prev = curr;
-            curr = next;
-            System.out.println(prev);
+        Scanner num = new Scanner(System.in);
+        System.out.println("Enter number of members of the sequence: ");
+
+        if (!num.hasNextInt() || n<=0) {
+            System.out.println("No results");
+        }
+        else {
+            n = num.nextInt();
+            for (int i = 0; i < n; i++) {
+                    int next = prev + curr;
+                    prev = curr;
+                    curr = next;
+
+                    System.out.println(prev);
+                }
+            }
         }
     }
-}
+
