300bbead3dfa86a7b1396655bd57cde78e95ec89
vitalina <vitalina.zubko@mev.com>
2018-04-11 12:06:48 +0300

    phone validator and sum calculating

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,55 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Vitalina");
+
+        System.out.println("Enter your numbers:");
+
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int sum = 0;
+        iAmSum(num, sum);
+    }
+
+    public static void iAmSum(int num, int sum) {
+        if (num > 0) {
+            sum += num % 10;
+            iAmSum(num / 10, sum);
+        } else
+
+            switch (sum) {
+                case 1:
+                    System.out.println("One");
+                    break;
+                case 2:
+                    System.out.println("Two");
+                    break;
+                case 3:
+                    System.out.println("Three");
+                    break;
+                case 4:
+                    System.out.println("Four");
+                    break;
+                case 5:
+                    System.out.println("Your sum is " + sum);
+                    break;
+                case 6:
+                    System.out.println("Your sum is " + sum);
+                    break;
+                case 7:
+                    System.out.println("Your sum is " + sum);
+                    break;
+                case 8:
+                    System.out.println("Your sum is " + sum);
+                    break;
+                case 9:
+                    System.out.println("Your sum is " + sum);
+                    break;
+                default:
+                    iAmSum(sum,0);
+                    break;
+            }
     }
 }
--- src/homeTasks/PhoneValidator.java
+++ src/homeTasks/PhoneValidator.java
@@ -1,11 +1,11 @@
 package homeTasks;
 
-import java.lang.reflect.Executable;
+import com.company.Main;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PhoneValidator {
+public class PhoneValidator extends Main{
 
     public static void main(String[] argv) {
 
@@ -21,11 +21,13 @@ public class PhoneValidator {
             try {
             System.out.println("Please, enter valid phone number: ");
                 num.nextInt();
+
             break;
                  } catch (Exception e) {
                 num.nextLine();
-
             }
         }
+
+
     }
 }
