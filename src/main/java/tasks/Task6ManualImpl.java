2132106ab99ee27a56f00059812f1e3cb1a5d4d0
vitalina <vitalina.zubko@mev.com>
2019-08-15 00:37:49 +0300

    fixed task 6 with manual implementation and built-in functions

---
+++ src/main/java/tasks/Task6ManualImpl.java
@@ -0,0 +1,20 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class Task6ManualImpl {
+    public static void main(String[] args) {
+        System.out.println("Please enter a sentence:");
+        Scanner w = new Scanner(System.in);
+        String strInput = w.nextLine();
+        String withoutSpaces = "";
+
+            for (int i = 0; i < strInput.length(); i++) {
+                if (strInput.charAt(i) != ' ')
+                    withoutSpaces += strInput.charAt(i);
+            }
+
+        System.out.print("Your sentence: " + strInput + "\n");
+        System.out.print("Your sentence without spaces: " + withoutSpaces);
+    }
+}
---
+++ src/main/java/tasks/Task6WithBuiltInFunctions.java
@@ -0,0 +1,16 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class Task6WithBuiltInFunctions {
+    public static void main(String[] args) {
+        System.out.println("Please enter a sentence:");
+        Scanner w = new Scanner(System.in);
+        String strInput = w.nextLine();
+
+        String strOutput = strInput.replaceAll(" ", "");
+
+        System.out.print("Your sentence: " + strInput + "\n");
+        System.out.print("Your sentence without spaces: " + strOutput);
+    }
+}
