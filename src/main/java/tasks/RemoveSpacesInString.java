2f09a64f9a6e9d8c4055bc75067743fae017cfc3
vitalina <vitalina.zubko@mev.com>
2019-09-08 22:19:25 +0300

    removed extra files

--- src/main/java/tasks/RemoveSpacesInString.java
+++
@@ -1,9 +0,0 @@
-package tasks;
-public class RemoveSpacesInString {
-        public static void main(String args[]){
-            String strInput = new String(" This is a small task for removing spaces in the sentence ");
-            String strOutput = strInput.replaceAll(" ", "");
-
-            System.out.print("Result: " + strOutput);
-        }
-}
